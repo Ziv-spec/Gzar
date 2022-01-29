d2801b115614486cc69946b0dc9ea02f847a686e
Ziv-spec <natalyka96@gmail.com>
2022-01-29 17:15:04 +0200

    trying to make the .gitignore work

--- build.bat
+++
@@ -1,8 +0,0 @@
-@echo off 
-IF NOT EXIST build mkdir build 
-
-set LinkerOptions=/OUT:gzar.exe /incremental:no
-set WarningOptions= -wd4706
-pushd build 
-cl ../src/main.c -nologo /Zi /FC %WarningOptions% /link %LinkerOptions%
-popd
